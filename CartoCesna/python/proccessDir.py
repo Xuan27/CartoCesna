004a0966900d28b76eb2b6510a5db284abe26e01
Neil Gibeaut <ncg19@txstate.edu>
2016-07-27 15:37:42 -0500

    added python scripts

---
+++ CartoCesna/python/proccessDir.py
@@ -0,0 +1,8 @@
+import os, subprocess
+
+
+for f in os.listdir('.'):
+    if os.path.isfile(f):
+		extension =  f[-3:]
+		if extension == "tif":
+			subprocess.call(["python", "addGeometry2DB.py", f])
