e7dfbd0bf3f00e2c6130e10ef857f8b46acc69a2
Xuan27 <jmshebar259@gmail.com>
2016-07-27 09:46:50 -0500

    CartoCesna Spatial Query
    Introduction to the spatial query web tool for the Scanning Project

---
+++ CartoCesna/Javascript/Spatial_Query.js
@@ -0,0 +1,2 @@
+var map = L.mapbox.map("mapid", "mapbox.streets");
+L.mapbox.accessToken = "pk.eyJ1IjoieHVhbjI3IiwiYSI6IktzT0hVNjAifQ.v97O2GRYRJ8ZxhLHtTn30g"
---
+++ CartoCesna/PHP/PHPGeoJSON.php
@@ -0,0 +1,46 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "spatial_query";
+
+//Create connection
+$conn = new mysqli($servername, $username, $password, $database);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+//echo "Connected successfully";
+/*$Table = "CREATE TABLE geom2 (Filename VARCHAR(30) NOT NULL, g GEOMETRY)";
+if ($conn->query($Table) === TRUE) {
+    echo "Table MyGuests created successfully";
+} else {
+    echo "Error creating table: " . $conn->error;
+}*/
+
+/*$Insert = "INSERT INTO geom2 (Filename, g) VALUES ('11.tif', ST_GeomFromText('POINT(-97.5858916 27.5570899)'))";
+if ($conn->query($Insert) === TRUE) {
+   // echo "New record created successfully";
+} else {
+    echo "Error: " . $Insert . "<br>" . $conn->error;
+}*/
+
+
+$sql = "SELECT ST_AsGeoJSON(g) FROM geom2";
+
+$result = $conn->query($sql);
+
+$GeoJson = array();
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+		$GeoJson[] = $row;		
+    }
+} else {
+    echo "0 results";
+}
+echo json_encode($GeoJson);
+
+$conn->close();
+?>
