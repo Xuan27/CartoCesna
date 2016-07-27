0af2e0c11f31e3c0ca2ad1873fdb1e44a341f9e8
Xuan27 <jmshebar259@gmail.com>
2016-07-27 11:31:43 -0500

    Mysql Python Geotiff info
    Python script that gets the coordinates of the geotiff from gdal info in
    the command line and saves them to the spatial query database .

--- CartoCesna/GeoTIFFs/Data.py
+++ CartoCesna/GeoTIFFs/Data.py
@@ -4,7 +4,7 @@
 #
 # Author:      jmartinez91
 #
-# Created:     25/07/2016
+# Created:     27/07/2016
 # Copyright:   (c) jmartinez91 2016
 # Licence:     <your licence>
 #-------------------------------------------------------------------------------
@@ -26,23 +26,34 @@ for dirpath, dirnames, filenames in os.walk("C:\wamp\www\CartoCesna\GeoTIFFs", "
         data = json.loads(output)
 
         Centroid = data["cornerCoordinates"]["center"]
+        Polygon_upper_Right_X = data["cornerCoordinates"]["upperRight"][0]
+        Polygon_upper_Right_Y = data["cornerCoordinates"]["upperRight"][1]
+
+        Polygon_lowerLeft_X = data["cornerCoordinates"]["lowerLeft"][0]
+        Polygon_lowerLeft_Y = data["cornerCoordinates"]["lowerLeft"][1]
+
+        Polygon_lower_Right_X = data["cornerCoordinates"]["lowerRight"][0]
+        Polygon_lower_Right_Y = data["cornerCoordinates"]["lowerRight"][1]
+
+        Polygon_upper_Left_X = data["cornerCoordinates"]["upperLeft"][0]
+        Polygon_upper_Left_Y = data["cornerCoordinates"]["upperLeft"][1]
+        #print Polygon_upper_Right_X, Polygon_upper_Right_Y, Polygon_lowerLeft_X, Polygon_lowerLeft_Y, Polygon_lower_Right_X, Polygon_lower_Right_Y, Polygon_upper_Left_X, Polygon_upper_Left_Y
         Filename= filenames
 
+       db= mysql.connector.connect(host = "localhost", user = "root", passwd = "", db = "spatial_query")
+       cursor = db.cursor()
 
-        db= mysql.connector.connect(host = "localhost", user = "root", passwd = "", db = "spatial_query")
-        cursor = db.cursor()
+       add_json = ("""INSERT INTO geom2 (Filename, g, Poly) VALUES (%s, ST_GeomFromText('POINT(%s %s)'), ST_GeomFromText('POLYGON((%s %s,%s %s,%s %s,%s %s, %s %s))'))""")
 
-        add_json = ("""INSERT INTO geom2 (Filename, g) VALUES (%s, ST_GeomFromText('POINT(%s %s)'))""")
+#INSERT INTO geom2 (Filename, g, Poly) VALUES (%s, ST_GeomFromText('POINT(%s %s)'), GeomFromText('POLYGON((%s %s,%s %s,%s %s,%s %s))'))
 
+    Coordinates = Centroid
+    Centx = Coordinates[0]
+    Centy = Coordinates[1]
+    info= (Filename, Centx, Centy, Polygon_upper_Left_X, Polygon_upper_Left_Y, Polygon_upper_Right_X, Polygon_upper_Right_Y, Polygon_lower_Right_X, Polygon_lower_Right_Y, Polygon_lowerLeft_X, Polygon_lowerLeft_Y, Polygon_upper_Left_X, Polygon_upper_Left_Y)
+    print info
 
-
-        Coordinates = Centroid
-        Centx = Coordinates[0]
-        Centy = Coordinates[1]
-        info= (Filename, Centx, Centy)
-        print info
-
-        cursor.execute(add_json, info)
-        db.commit()
+    cursor.execute(add_json, info)
+    db.commit()
 
 db.close()
