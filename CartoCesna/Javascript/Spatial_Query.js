94b3378ac443dff5adc6030149c4fcc17e6a0da6
Neil Gibeaut <ncg19@txstate.edu>
2016-07-28 09:57:56 -0500

    Improved UI
    fixed multiple bugs and adjusted some of the formatting

--- CartoCesna/Javascript/Spatial_Query.js
+++ CartoCesna/Javascript/Spatial_Query.js
@@ -2,17 +2,15 @@ function deleteRectangle()
 {	
 	if(markerCount == 2)
 	{
-		map.removeLayer(rectangleArray[rectangleArray.length-1]);
-		rectangleArray.splice(rectangleArray.length, 1);
+		map.removeLayer(tempRectangleArray[tempRectangleArray.length-1]);
+		tempRectangleArray = [];
 		rectangleCoords.splice(rectangleCount-1, 2);
 		rectangleCount--;
 		
-		map.removeLayer(markerArray[markerCount-1]);
-		map.removeLayer(markerArray[markerCount-2]);
-		markerArray.splice(markerCount-2, 2);
+		map.removeLayer(tempMarkerArray[markerCount-1]);
+		map.removeLayer(tempMarkerArray[markerCount-2]);
+		tempMarkerArray.splice(markerCount-2, 2);
 		markerCount = markerCount-2;
-		
-
 	}
 	else if(markerCount == 1)
 	{
@@ -27,37 +25,55 @@ function deleteRectangle()
 }
 
 function submitQuery(boundingBox)
-{
+{	
+	deleteTable("resultsTable");
+	for(var i = 0; i < rectangleArray.length; i++)
+	{
+		map.removeLayer(rectangleArray[i]);
+		map.removeLayer(markerArray[i]);
+	}
+	rectangleArray = [];
+	markerArray = [];
+	
 	diagonalCoords = addDiagonalObject(boundingBox.getBounds()._southWest.lat,
 														boundingBox.getBounds()._southWest.lng, 
 														boundingBox.getBounds()._northEast.lat, 
 														boundingBox.getBounds()._northEast.lng)
 	deleteRectangle();
-														
-	//alert(diagonalCoords.y2);
+	deleteTable("resultsTable");
+	for(var i = 0; i < rectangleArray.length; i++)
+	{
+		map.removeLayer(rectangleArray[i]);
+		map.removeLayer(markerArray[i]);
+	}
 	
 	$.ajax({
 		type: 'post',
 		url: '../PHP/submitQuery.php',
 		data:{'diagonalCoords':JSON.stringify(diagonalCoords)},
 		success: function(data){
+			if(data == "0 results[]")
+			{
+				alert("No Matches Found");
+				return;				
+			}
 			drawResults(JSON.parse(data));
 			displayLinks(JSON.parse(data));
- 			//L.geoJson(geojson).addTo(map);
 		}
 	});
 }
 
 function drawResults(results)
 {
-
-	var rectangleArray = new Array();
-	var markerArray = new Array();
+	Maki_Icon = icon = L.MakiMarkers.icon({color: "#FF0000" , size: "m"});
+	Maki_Icon = defaultIcon = L.MakiMarkers.icon({color: "#58d68d" , size: "m"});
+	//var rectangleArray = new Array();
+	//var markerArray = new Array();
 	
 	for(var i = 0; i < results.length; i++)
 	{	
 		var rectangle = L.rectangle([[results[i].y1, results[i].x1],
-		[results[i].y2, results[i].x2]],{color: "	 #58d68d", weight: 1});
+		[results[i].y2, results[i].x2]],{fillOpacity: .05, color: "#58d68d", weight: 3});
 		rectangleArray.push(rectangle);
 		map.addLayer(rectangleArray[i]);
 		
@@ -67,8 +83,34 @@ function drawResults(results)
 		var centerX = (parseFloat(results[i].x1) + parseFloat(results[i].x2))*.5;
 		
 		var marker = L.marker([centerY, centerX]);
-		markerArray.push(marker);
+		marker.bindPopup("" + i + "").openPopup();
+		marker.setIcon(defaultIcon);
+		
+		markerArray.push(marker);	
 		map.addLayer(markerArray[i]);
+		table = document.getElementById("resultsTable");
+		
+		markerArray[i].on('click', function(e)
+		{
+			this.closePopup();
+			var highlight = {color: "#FF0000", weight: 1};
+			var defaultColor = {color: "#58d68d", weight: 1};		
+		
+			var popup = this.getPopup();
+
+			for(var i = 0; i < markerArray.length; i++)
+			{
+				rectangleArray[i].setStyle(defaultColor);
+				markerArray[i].setIcon(defaultIcon);
+				table.rows[i+1].style.backgroundColor = "#e5f1fd";
+			}
+			
+			rectangleArray[popup.getContent()].setStyle(highlight)
+			this.setIcon(icon);
+			
+			highlightTable(popup.getContent());
+		});
+
 	}
 }
 
@@ -85,9 +127,13 @@ function displayLinks(results)
 		cell1.innerHTML = fileName.substring(0, fileName.length-4) + "<br><br>";
 		cell2.innerHTML = "<a href = '" + results[i].kmz + "'>" + "kmz" + "</a>" + ", <a href = '" + results[i].GeoTIFF + "'>" + "GeoTIFF" + "</a>"  + "<br><br>"	;		
 	}
+}
 
-
-
+function highlightTable(index)
+{ 
+	index++;
+	table = document.getElementById("resultsTable");
+	table.rows[index].style.backgroundColor = '#FFFFE0';
 }
 
 function addDiagonalObject(x1, y1, x2, y2)
@@ -104,7 +150,6 @@ function diagonalObjectConstructor(x1, y1, x2, y2)
 	this.y2 = y2;
 }
 
-
 function deleteTable(id)
 {
 	var table = document.getElementById(id);
@@ -119,9 +164,9 @@ function deleteTable(id)
 	deleteTable(id);
 }
 
-
-
-
+//global variables
+rectangleArray = new Array();
+markerArray = new Array();
 
 
 
