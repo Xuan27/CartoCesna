640c4c06087d3ce1c444e03e32d161b3df0dc094
Neil Gibeaut <ncg19@txstate.edu>
2016-08-08 12:16:55 -0500

    added documentation

--- Javascript/Spatial_Query.js
+++ Javascript/Spatial_Query.js
@@ -1,11 +1,11 @@
 //Global Variables
-rectangleArray = new Array();
-markerArray = new Array();
-var spatialQuerySelection = "intersects";
-var dateQuerySelection = "allYears";
-var dateQuerySQL = "";
-var highlight = {color: "#FF0000", weight: 1};
-var defaultColor = {color: "#58d68d", weight: 1};	
+rectangleArray = new Array(); 								//contains all the rectangles created from a succesful query
+markerArray = new Array();									//contains all the markers created from a succesful query
+var spatialQuerySelection = "intersects";					//string of used to decide what spatial query technique will be used 
+var dateQuerySelection = "allYears";						//string used to decide what date query technique will be used 
+var dateQuerySQL = "";										//string of SQL statement to refine search by date
+var highlight = {color: "#FF0000", weight: 1};			//higlight properties of markers/rectangles.
+var defaultColor = {color: "#58d68d", weight: 1};		//default properties of markers/rectangles.
 
 //This function is used to delete the active rectangle that the user has drawn.
 //This function is currently not in use, However I'm leaving it here in case in the future the two click method
@@ -190,7 +190,7 @@ function highlightMapMarker(index)
 	}
 	rectangleArray[index].setStyle(highlight);
 	markerArray[index].setIcon(icon);
-
+	
 	highlightTable(index);
 	map.fitBounds(rectangleArray[index].getLatLngs(), {padding: [50, 50]}, {animate: true});
 }
@@ -216,6 +216,7 @@ function deleteTable(id)
 		return;
 	
 	table.deleteRow(table.rows.length-1);
+	document.getElementById("subHeader").innerHTML = "";
 	
 	//recursive call 
 	deleteTable(id);
--- PHP/submitQuery.php
+++ PHP/submitQuery.php
@@ -1,97 +1,102 @@
 <?php
-$diagonalCoords = json_decode($_POST['diagonalCoords']);
-$x1 = $diagonalCoords -> x1;
-$y1 = $diagonalCoords -> y1;
-$x2 = $diagonalCoords -> x2;
-$y2 = $diagonalCoords -> y2;
-$spatialQuerySelection = $diagonalCoords -> spatialQuerySelection;
-$dateQuerySQL = $diagonalCoords -> dateQuerySQL;
-$authorSQL = $diagonalCoords -> authorSQL;
+/* 	This script is called when a user submits a query. The script uses all the data that it 
+	recieves from the AJAX call in the submitQuery() function and performs the appropriate SQL query
+	on the database based on the content of that POST. The script returns all the results that match
+	the query in JSON format. */
+	
+	$queryObject = json_decode($_POST['queryObject']);
+	$x1 = $queryObject -> x1;
+	$y1 = $queryObject -> y1;
+	$x2 = $queryObject -> x2;
+	$y2 = $queryObject -> y2;
+	$spatialQuerySelection = $queryObject -> spatialQuerySelection;
+	$dateQuerySQL = $queryObject -> dateQuerySQL;
+	$authorSQL = $queryObject -> authorSQL;
 
-$servername = "localhost";
-$username = "neilgibeaut";
-$password = "Sadiedog1995";
-$database = "spatialSearch_db";
+	$servername = "localhost";
+	$username = "neilgibeaut";
+	$password = "Sadiedog1995";
+	$database = "spatialSearch_db";
 
-//Create connection
-$conn = new mysqli($servername, $username, $password, $database);
+	//Create connection
+	$conn = new mysqli($servername, $username, $password, $database);
 
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-//echo "Connected successfully";
+	// Check connection
+	if ($conn->connect_error) {
+		die("Connection failed: " . $conn->connect_error);
+	}
+	//echo "Connected successfully";
 
-if($spatialQuerySelection == "intersects")
-{
-$sql = "SELECT fileName, x1, y1, x3, y3, kmz, Author
-			FROM table1
-			WHERE MBRIntersects(
-			GeomFromText( 'LINESTRING($y1 $x1, $y2 $x2)' ),
-			table1.diagonal) $dateQuerySQL $authorSQL";
+	if($spatialQuerySelection == "intersects")
+	{
+	$sql = "SELECT fileName, x1, y1, x3, y3, kmz, Author
+				FROM table1
+				WHERE MBRIntersects(
+				GeomFromText( 'LINESTRING($y1 $x1, $y2 $x2)' ),
+				table1.diagonal) $dateQuerySQL $authorSQL";
 
-$result = $conn->query($sql);
+	$result = $conn->query($sql);
 
-$GeoJson = array();
-if ($result->num_rows > 0) {
-    // output data of each row
-    while($row = $result->fetch_assoc()) {
-		$GeoJson[] = $row;		
-    }
-} else {
-    echo "0 results";
-}
-echo json_encode($GeoJson);
+	$GeoJson = array();
+	if ($result->num_rows > 0) {
+		// output data of each row
+		while($row = $result->fetch_assoc()) {
+			$GeoJson[] = $row;		
+		}
+	} else {
+		echo "0 results";
+	}
+	echo json_encode($GeoJson);
 
-$conn->close();	
-}
+	$conn->close();	
+	}
 
-if($spatialQuerySelection == "contains")
-{
-$sql = "SELECT fileName, x1, y1, x3, y3, kmz, Author
-			FROM table1
-			WHERE MBRContains(
-			GeomFromText( 'LINESTRING($y1 $x1, $y2 $x2)' ),
-			table1.diagonal) $dateQuerySQL $authorSQL";
+	if($spatialQuerySelection == "contains")
+	{
+	$sql = "SELECT fileName, x1, y1, x3, y3, kmz, Author
+				FROM table1
+				WHERE MBRContains(
+				GeomFromText( 'LINESTRING($y1 $x1, $y2 $x2)' ),
+				table1.diagonal) $dateQuerySQL $authorSQL";
 
-$result = $conn->query($sql);
+	$result = $conn->query($sql);
 
-$GeoJson = array();
-if ($result->num_rows > 0) {
-    // output data of each row
-    while($row = $result->fetch_assoc()) {
-		$GeoJson[] = $row;		
-    }
-} else {
-    echo "0 results";
-}
-echo json_encode($GeoJson);
+	$GeoJson = array();
+	if ($result->num_rows > 0) {
+		// output data of each row
+		while($row = $result->fetch_assoc()) {
+			$GeoJson[] = $row;		
+		}
+	} else {
+		echo "0 results";
+	}
+	echo json_encode($GeoJson);
 
-$conn->close();	
-}
+	$conn->close();	
+	}
 
-if($spatialQuerySelection == "containsCentroid")
-{
-$sql = "SELECT fileName, x1, y1, x3, y3, kmz, Author
-			FROM table1
-			WHERE MBRContains(
-			GeomFromText( 'LINESTRING($y1 $x1, $y2 $x2)' ),
-			table1.centroid) $dateQuerySQL $authorSQL";
+	if($spatialQuerySelection == "containsCentroid")
+	{
+	$sql = "SELECT fileName, x1, y1, x3, y3, kmz, Author
+				FROM table1
+				WHERE MBRContains(
+				GeomFromText( 'LINESTRING($y1 $x1, $y2 $x2)' ),
+				table1.centroid) $dateQuerySQL $authorSQL";
 
-$result = $conn->query($sql);
+	$result = $conn->query($sql);
 
-$GeoJson = array();
-if ($result->num_rows > 0) {
-    // output data of each row
-    while($row = $result->fetch_assoc()) {
-		$GeoJson[] = $row;		
-    }
-} else {
-    echo "0 results";
-}
-echo json_encode($GeoJson);
+	$GeoJson = array();
+	if ($result->num_rows > 0) {
+		// output data of each row
+		while($row = $result->fetch_assoc()) {
+			$GeoJson[] = $row;		
+		}
+	} else {
+		echo "0 results";
+	}
+	echo json_encode($GeoJson);
 
-$conn->close();	
-}
+	$conn->close();	
+	}
 
 ?>
